7c7abbe0d807a0b4c610e3d7faa4e69d97a69ce8
ssstrieu <samanthastrieu@gmail.com>
2015-10-24 13:03:06 -0700

    forms placeholder

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+$('.addStock').on('click', function(){
+    $('.addStock').prepend(<div class='col-md-2'><lable for='symbol' class='labels'>Ticker Symbol</lable><input type="text" class='form-control' name="symbol" placeholder=""></div>
+
+
+})
