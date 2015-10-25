f2980ed284f080ccaf4bfc525a1bfed01b2d723a
Yinyin W <xiaodian.wang@gmail.com>
2015-10-24 20:48:25 -0700

    integrated api calls with frontend

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import requests
+import stock
 from flask import Flask, request, redirect, render_template, session
-
+from werkzeug.datastructures import ImmutableMultiDict
 
 app = Flask(__name__) 
 
 @app.route('/')
 def index():
     signed_in= False
-    return render_template('index.html', )
+    return render_template('index.html')
     
+@app.route('/compare', methods=['POST'])
+def compare():
+	print '\n****POSTed Form Content******\n'
+	print request.form.getlist('symbol')
+	symbols = stock.unicode_to_str(request.form.getlist('symbol'))
+	results = stock.stock_results(symbols) 
+	return render_template('index.html', quotes=results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- stock.py
+++ stock.py
@@ -4,6 +4,8 @@ import urllib
 # Builds the Yahoo API request URL, given a list of stock symbols, i.e. ["YHOO", "FB"]
 # symbol list needs to be at least size 1
 def stock_url(symbols):
+	print '\n******stock_url input******\n'
+	print symbols
 	SEARCH_URL = 'https://query.yahooapis.com/v1/public/yql?'
 	params = {
 		'q': 'select * from yahoo.finance.quotes where symbol in {}'.format(format_symbols(symbols)),
@@ -12,6 +14,8 @@ def stock_url(symbols):
 		'env': 'store://datatables.org/alltableswithkeys'
 	}
 	stock_request = SEARCH_URL+urllib.urlencode(params)
+	print '\n ******Request URL****** \n'
+	print stock_request
 	return stock_request
 	
 # Helper function to stock_req_url()
@@ -23,13 +27,25 @@ def format_symbols(symbols):
 	strng = strng.replace(']', ')')
 	return strng
 
+# Helper function to convert a list of unicode strings
+# to a list of regular strings
+def unicode_to_str(lst):
+	string_lst = []
+	for el in lst:
+		string_lst.append(str(el))
+	return string_lst
+
+
 # Given a list of stock symbols, returns the API response in JSON
 def stock_results(symbols):
 	result = requests.get(stock_url(symbols)).json()
 	quote = result['query']['results']['quote']
 	if type(quote) is dict:
+		print '\n****RESPONSE******\n'
+		print [quote]
 		return [quote]
 	else:
 		return quote
+
 
 
