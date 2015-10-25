5a4e4ce0ea781961a1c263b617c07b201c196ff5
ssstrieu <samanthastrieu@gmail.com>
2015-10-24 20:02:58 -0700

    removed unique ID for symbol input

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 var addCount=0;
 $( ".addStock" ).click(function() {
-    $('.btnContainer').before("<div class='col-md-2'><lable for='symbol"+(addCount+3)+"' class='labels'>Ticker Symbol</lable><input type='text' class='form-control' name='symbol"+(addCount+3)+"' placeholder='NEW'></div>")
+    $('.btnContainer').before("<div class='col-md-2'><lable for='symbol' class='labels'>Ticker Symbol</lable><input type='text' class='form-control' name='symbol' placeholder='NEW'></div>")
     addCount++;
     console.log(addCount);
     if (addCount>2){
